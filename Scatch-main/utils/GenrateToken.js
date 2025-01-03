0ebe62844253123e38817e51cb020059f7148f36
Amrit Krishn <163405281+Gitamrit@users.noreply.github.com>
2025-01-03 23:34:20 +0530

    Delete Scatch-main directory

--- Scatch-main/utils/GenrateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-
-const token = (user)=>{
-   return jwt.sign({email:user.email , id:user._id}, process.env.JWT_KEY);
-}
-
-module.exports.jsontoken = token;
